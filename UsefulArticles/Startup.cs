e330a07611ca222d040cbc565208ffbfdab3c415
Giocatory <nivolyas@gmail.com>
2022-03-26 20:51:11 +0300

    Добавил пакеты, базовая настройка, создание контроллера, представление

--- UsefulArticles/Startup.cs
+++ UsefulArticles/Startup.cs
@@ -4,21 +4,26 @@
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            // Добавляем поддержку контроллеров и представлений (mvc)
+            services.AddControllersWithViews().AddSessionStateTempDataProvider();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // порядок регистрации middleware очень важен!!!!!!!
+
+            // для отображения ошибок в процессе разработки
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
             app.UseRouting();
+
+            // подключение поддержки статичных файлов в приложении (css, js ...) папка wwwroot
+            app.UseStaticFiles();
+
+            // регистрация маршрутов (endpoints)
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("<h1>Hello Mikle</h1>");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
