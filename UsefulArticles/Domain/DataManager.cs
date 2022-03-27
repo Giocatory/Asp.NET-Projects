978d90d69f573dc07819d355e560e7c2d253c1f0
Giocatory <nivolyas@gmail.com>
2022-03-27 20:05:38 +0300

    класс помошник для управления репозиторием

---
+++ UsefulArticles/Domain/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using UsefulArticles.Domain.Repositories.Abstract;
+
+namespace UsefulArticles.Domain
+{
+    public class DataManager
+    {
+        public ITextFieldsRepository? TextFields { get; set; }
+        public IServiceItemsRepository? ServiceItems { get; set; }
+
+        public DataManager(ITextFieldsRepository textFieldsRepository, IServiceItemsRepository serviceItemsRepository)
+        {
+            TextFields = textFieldsRepository;
+            ServiceItems = serviceItemsRepository;
+        }
+    }
+}
