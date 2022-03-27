1106863a8810263704183bff114a9a21877a321d
Giocatory <nivolyas@gmail.com>
2022-03-27 18:59:42 +0300

    Контекст базы данных

---
+++ UsefulArticles/Domain/AppDbContext.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UsefulArticles.Domain.Entities;
+
+namespace UsefulArticles.Domain
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TextField>? TextFields { get; set; }
+        public DbSet<ServiceItem>? ServiceItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = "8af10569-b018-4fe7-a380-7d6a14c70b74",
+                Name = "admin",
+                NormalizedName = "ADMIN"
+            });
+
+            modelBuilder.Entity<IdentityUser>().HasData(new IdentityUser
+            {
+                Id = "3b62472e-4f66-49fa-a20f-e7685b9565d8",
+                UserName = "admin",
+                NormalizedUserName = "ADMIN",
+                Email = "giocatory@yandex.ru",
+                NormalizedEmail = "GIOCATORY@YANDEX.RU",
+                EmailConfirmed = true,
+                PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null!, "IPSequence*351"),
+                SecurityStamp = string.Empty
+            });
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = "8af10569-b018-4fe7-a380-7d6a14c70b74",
+                UserId = "3b62472e-4f66-49fa-a20f-e7685b9565d8"
+            });
+
+            modelBuilder.Entity<TextField>().HasData(new TextField
+            {
+                Id = new Guid("63dc8fa6-07ae-4391-8916-e057f71239ce"),
+                CodeWord = "PageIndex",
+                Title = "Главная"
+            });
+
+            modelBuilder.Entity<TextField>().HasData(new TextField
+            {
+                Id = new Guid("70bf165a-700a-4156-91c0-e83fce0a277f"),
+                CodeWord = "PageArticles",
+                Title = "Мои статьи"
+            });
+
+            modelBuilder.Entity<TextField>().HasData(new TextField
+            {
+                Id = new Guid("4aa76a4c-c59d-409a-84c1-06e6487a137a"),
+                CodeWord = "PageContacts",
+                Title = "Контакты"
+            });
+        }
+    }
+}
