6da15ddb9352589a35a57aa14d5b701bdfd395ac
Giocatory <nivolyas@gmail.com>
2022-03-27 21:35:28 +0300

    Конфиг от руки к апп сеттингу не конектится

--- UsefulArticles/Service/Config.cs
+++ UsefulArticles/Service/Config.cs
@@ -4,8 +4,8 @@
     {
         public static string? ConnectionString { get; set; } = "Data Source=localhost\\SQLEXPRESS; Database=UsefulArticles; Persist Security Info=false; User ID='giocatory'; Password='IPSequence*351'; MultipleActiveResultSets=True; Trusted_Connection=False;";
         public static string CompanyName { get; set; } = "Giocatory - Useful Articles";
-        public static string CompanyPhone { get; set; } = "";
-        public static string CompanyPhoneShort { get; set; } = "";
-        public static string CompanyEmail { get; set; } = "";
+        public static string CompanyPhone { get; set; } = "+7 (914) 050-34-17";
+        public static string CompanyPhoneShort { get; set; } = "+79140503417";
+        public static string CompanyEmail { get; set; } = "giocatory@yandex.ru";
     }
 }
