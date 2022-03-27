5fee6cd1d4224ee6815026eb97d009b317d8cd53
Giocatory <nivolyas@gmail.com>
2022-03-27 18:26:35 +0300

    доменные объекты, для проецирования в БД

---
+++ UsefulArticles/Domain/Entities/ServiceItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsefulArticles.Domain.Entities
+{
+    public class ServiceItem : EntityBase
+    {
+        [Required(ErrorMessage = "Заполните название статьи")]
+        [Display(Name = "Название статьи")]
+        public override string? Title { get; set; }
+
+        [Display(Name = "Краткое описание статьи")]
+        public override string? Subtitle { get; set; }
+
+        [Display(Name = "Полное описание статьи")]
+        public override string? Text { get; set; }
+    }
+}
