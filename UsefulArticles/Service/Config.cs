4f917096f7be12271247f13120ad54788e0846b1
Giocatory <nivolyas@gmail.com>
2022-03-27 21:15:44 +0300

    Создание миграции

--- UsefulArticles/Service/Config.cs
+++ UsefulArticles/Service/Config.cs
@@ -2,7 +2,7 @@
 {
     public class Config
     {
-        public static string ConnectionString { get; set; } = "";
+        public static string? ConnectionString { get; set; } = "Data Source=(local)\\SQLSERVER; Database=UsefulArticles; Persist Security Info=false; User ID='giocatory'; Password='IPSequence*351'; MultipleActiveResultSets=True; Trusted_Connection=False;";
         public static string CompanyName { get; set; } = "Giocatory - Useful Articles";
         public static string CompanyPhone { get; set; } = "";
         public static string CompanyPhoneShort { get; set; } = "";
